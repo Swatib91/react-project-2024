38aedd4ea99dc980fb3ddf943c3fe889e90331e6
Swatib91 <21swatibhaip@gmail.com>
2024-01-22 14:58:43 +0400

    Episode 1 - React introduction

---
+++ app.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement("h1", { id: "heading" }, "HEllo World from React!");
+// console.log(heading);
+
+
+const parent = React.createElement("div", { id: "paraent" },
+    [
+        React.createElement("div", { id: "child" },
+            [React.createElement("h1", {}, "Hi, I m in H1 tag"),
+            React.createElement("h2", {}, "Hi, I m in H2 tag")]
+        ),
+        React.createElement("div", { id: "child" },
+            [React.createElement("h1", {}, "Hi, I m in H1 tag"),
+            React.createElement("h2", {}, "Hi, I m in H2 tag")]
+        )]
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent); // putting this parent inside this root tag and replace everything inside roots(if any other tag already inside <div>)
