3e306611e328609b788f6f08dd7027e3ad698aff
Swatib91 <21swatibhaip@gmail.com>
2024-01-24 11:12:33 +0400

    React: Igniting our app

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // const heading = React.createElement("h1", { id: "heading" }, "HEllo World from React!");
 // console.log(heading);
 
@@ -6,7 +9,7 @@ const parent = React.createElement("div", { id: "paraent" },
     [
         React.createElement("div", { id: "child" },
             [React.createElement("h1", {}, "Hi, I m in H1 tag"),
-            React.createElement("h2", {}, "Hi, I m in H2 tag")]
+            React.createElement("h2", {}, "This is react")]
         ),
         React.createElement("div", { id: "child" },
             [React.createElement("h1", {}, "Hi, I m in H1 tag"),
